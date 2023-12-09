df287d610b3c200c488f1842e2325caa6382b051
Roma <138378823+Cubball@users.noreply.github.com>
2023-12-09 19:29:32 +0200

    Sovle day 9 second task

---
+++ 2023/Day9/SecondTaskSolution.cs
@@ -0,0 +1,41 @@
+namespace AdventOfCode2023.Day9;
+
+public static class SecondTaskSolution
+{
+    public static int Initial(string[] input)
+    {
+        var sequences = input
+            .Select(l => l
+                        .Split(' ')
+                        .Select(int.Parse)
+                        .Reverse()
+                        .ToArray());
+        return sequences.Sum(GetPredictedValue);
+    }
+
+    private static int GetPredictedValue(int[] sequence)
+    {
+        var currentIndex = sequence.Length - 1;
+        while (true)
+        {
+            var allZeros = true;
+            for (int i = 0; i < currentIndex; i++)
+            {
+                if (sequence[i] != 0)
+                {
+                    allZeros = false;
+                }
+
+                sequence[i] = sequence[i + 1] - sequence[i];
+            }
+
+            currentIndex--;
+            if (allZeros)
+            {
+                break;
+            }
+        }
+
+        return sequence.Sum();
+    }
+}
--- 2023/Program.cs
+++ 2023/Program.cs
@@ -1,4 +1,4 @@
 ﻿using AdventOfCode2023.Day9;
 
 var input = File.ReadAllLines(@"Day9\Input.txt");
-Console.WriteLine(FirstTaskSolution.Initial(input));
+Console.WriteLine(SecondTaskSolution.Initial(input));
