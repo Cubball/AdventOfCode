87a07a989084df59242c4e516feaa4b0202577ce
Roma <138378823+Cubball@users.noreply.github.com>
2023-12-11 19:04:04 +0200

    Add 2 more solutions to day 11 part 1

--- 2023/Day11/FirstTaskSolution.cs
+++ 2023/Day11/FirstTaskSolution.cs
@@ -55,6 +55,115 @@ public static class FirstTaskSolution
         return sum;
     }
 
+    public static int WithoutBFS(string[] input)
+    {
+        var image = GetImage(input);
+        var galaxies = GetGalaxies(image);
+        var sum = 0;
+        while (galaxies.Count > 1)
+        {
+            var (row, col) = galaxies[^1];
+            for (int i = 0; i < galaxies.Count - 1; i++)
+            {
+                var (nextRow, nextCol) = galaxies[i];
+                sum += Math.Abs(row - nextRow) + Math.Abs(col - nextCol);
+            }
+
+            galaxies.RemoveAt(galaxies.Count - 1);
+        }
+
+        return sum;
+    }
+
+    public static int WithoutExpanding(string[] input)
+    {
+        var emptyRows = GetEmptyRows(input);
+        var emptyCols = GetEmptyColumns(input);
+        var galaxies = GetGalaxies(input);
+        var sum = 0;
+        while (galaxies.Count > 1)
+        {
+            var (row, col) = galaxies[^1];
+            for (int i = 0; i < galaxies.Count - 1; i++)
+            {
+                var (nextRow, nextCol) = galaxies[i];
+                var row1 = Math.Min(row, nextRow);
+                var row2 = Math.Max(row, nextRow);
+                var col1 = Math.Min(col, nextCol);
+                var col2 = Math.Max(col, nextCol);
+                var numberOfEmptyRows = GetNumberOfEmptyLinesBetween(row1, row2, emptyRows);
+                var numberOfEmptyCols = GetNumberOfEmptyLinesBetween(col1, col2, emptyCols);
+                sum += row2 - row1 + numberOfEmptyRows + col2 - col1 + numberOfEmptyCols;
+            }
+
+            galaxies.RemoveAt(galaxies.Count - 1);
+        }
+
+        return sum;
+    }
+
+    private static int GetNumberOfEmptyLinesBetween(int line1, int line2, HashSet<int> emptyLines)
+    {
+        var count = 0;
+        for (int i = line1 + 1; i < line2; i++)
+        {
+            if (emptyLines.Contains(i))
+            {
+                count++;
+            }
+        }
+
+        return count;
+    }
+
+    private static HashSet<int> GetEmptyRows(string[] input)
+    {
+        var set = new HashSet<int>();
+        for (int i = 0; i < input.Length; i++)
+        {
+            if (input[i].AsSpan().IndexOf('#') == -1)
+            {
+                set.Add(i);
+            }
+        }
+
+        return set;
+    }
+
+    private static List<(int Row, int Col)> GetGalaxies(string[] image)
+    {
+        var set = new List<(int, int)>();
+        for (int i = 0; i < image.Length; i++)
+        {
+            for (int j = 0; j < image[0].Length; j++)
+            {
+                if (image[i][j] == '#')
+                {
+                    set.Add((i, j));
+                }
+            }
+        }
+
+        return set;
+    }
+
+    private static List<(int Row, int Col)> GetGalaxies(List<char[]> image)
+    {
+        var set = new List<(int, int)>();
+        for (int i = 0; i < image.Count; i++)
+        {
+            for (int j = 0; j < image[0].Length; j++)
+            {
+                if (image[i][j] == '#')
+                {
+                    set.Add((i, j));
+                }
+            }
+        }
+
+        return set;
+    }
+
     private static void ClearVisited(bool[][] visited)
     {
         for (int i = 0; i < visited.Length; i++)
--- 2023/Program.cs
+++ 2023/Program.cs
@@ -2,3 +2,5 @@
 
 var input = File.ReadAllLines(@"Day11\Input.txt");
 Console.WriteLine(FirstTaskSolution.Initial(input));
+Console.WriteLine(FirstTaskSolution.WithoutBFS(input));
+Console.WriteLine(FirstTaskSolution.WithoutExpanding(input));
